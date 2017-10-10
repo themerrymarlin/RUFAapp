2c2aad498d4a4cf941d190391f0f6571d371fe50
themerrymarlin <dam134@case.edu>
2017-10-10 13:40:50 -0400

    added holder class to enable storing of multiple array types

---
+++ app/src/main/java/com/example/rufaapp/Holder.java
@@ -0,0 +1,31 @@
+package com.example.rufaapp;
+
+/**
+ * Created by David Merriman on 10/10/2017.
+ */
+
+class Holder<T> {
+    private T obj;
+    private int typeCode;
+
+    public Holder(T obj,int typeCode){
+        this.obj = obj;
+        this.typeCode = typeCode;
+    }
+
+    public T getObj(){
+        return this.obj;
+    }
+
+    public int getTypeCode(){
+        return this.typeCode;
+    }
+
+    public String getAsString(){
+        return (String)obj;
+    }
+
+    public Integer getAsInteger(){
+        return (Integer)obj;
+    }
+}
--- app/src/main/java/com/example/rufaapp/RUFAdbModifier.java
+++ app/src/main/java/com/example/rufaapp/RUFAdbModifier.java
@@ -15,12 +15,19 @@ public class RUFAdbModifier {
     private static final int NUMBER_OF_COLUMNS = 97;
     private static final String TABLE = "RUFA-DB";
 
-    public boolean insertRow(Context context, ArrayList<String> keys, ArrayList<String> values){
+    public boolean insertRow(Context context, ArrayList<String> keys, ArrayList<Holder<?>> values){
         RUFAdbOpenHelper openHelper = new RUFAdbOpenHelper(context);
         SQLiteDatabase db = openHelper.getWritableDatabase();
         ContentValues contentValues = new ContentValues(NUMBER_OF_COLUMNS);
         for(int i = 0; i < keys.size(); i ++){
-            contentValues.put(keys.get(i),values.get(i));
+            Holder<?> value = values.get(i);
+            int type = value.getTypeCode();
+            if(type == 0){
+                contentValues.put(keys.get(i),value.getAsString());
+            }else{
+
+                contentValues.put(keys.get(i),value.getAsInteger());
+            }
         }
         return db.insert(TABLE,null,contentValues) != -1L;
     }
