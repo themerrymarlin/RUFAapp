673313dba43c82c066de6db953886e60aa5abb75
themerrymarlin <dam134@case.edu>
2017-10-31 21:22:46 -0400

    preliminary csv file writer done, added library for csv accessibility

--- app/src/main/java/com/example/rufaapp/ExternalStorageAccessor.java
+++ app/src/main/java/com/example/rufaapp/ExternalStorageAccessor.java
@@ -2,6 +2,15 @@ package com.example.rufaapp;
 
 import android.os.Environment;
 
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOError;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Class to access SD card and
  * write CSV file to it, as well as potentially save
@@ -9,16 +18,36 @@ import android.os.Environment;
  */
 
 public class ExternalStorageAccessor {
+    String dir = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
+
     //TODO acquire write permissions in manifest.xml
-    public boolean isStorageWriteable(){
+    public boolean isStorageWriteable() {
         String state = Environment.getExternalStorageState();
         return Environment.MEDIA_MOUNTED.equals(state);
     }
 
-    public boolean isStorageReadable(){
+    public boolean isStorageReadable() {
         String state = Environment.getExternalStorageState();
         boolean rw = Environment.MEDIA_MOUNTED.equals(state);
         boolean r = Environment.MEDIA_MOUNTED_READ_ONLY.equals(state);
         return r || rw;
+    }
+
+    public void writeToCsv(ArrayList<String> keyList, ArrayList<Holder> values) throws IOException {
+        CSVWriter writer = new CSVWriter(new FileWriter(dir));
+        String fileName = values.get(0).getAsString() + values.get(1).getAsString() + ".csv";
+        String path = dir + File.separator + fileName;
+        File f = new File(path);
+        String[] row1 = new String[keyList.size()];
+        row1 = keyList.toArray(row1);
+        String[] row2 = new String[keyList.size()];
+        ArrayList<String> stringVals = new ArrayList<>(keyList.size());
+        for (Holder h : values) {
+            stringVals.add(h.getAsString());
+        }
+        row2 = stringVals.toArray(row2);
+        writer.writeNext(row1);
+        writer.writeNext(row2);
+        writer.close();
     }
 }
